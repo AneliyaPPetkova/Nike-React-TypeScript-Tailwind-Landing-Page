6452d0edc42c67f4527b83d95294825d5d0f5212
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-10-19 18:27:44 +0300

    Import container components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
 import "./App.css";
-
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./containers";
 const App = () => {
   return (
     <>
       <main className="relative">
         <h1 className="text-3xl font-bold underline">Nike App</h1>
         <section className="x1:padding-1 wide:padding-r padding-b">
-          Hero
+          <Hero />
         </section>
-        <section className="padding">Popular Products</section>
-        <section className="padding">Super Quality</section>
-        <section className="padding-x py-10">Services</section>
-        <section className="padding">Special Offer</section>
-        <section className="padding bg-pale-blue">Customer Reviews</section>
-        <section className="padding-x sm:py-32 w-full">Subscribe</section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+        <section className="padding-x py-10">
+          <Services />
+        </section>
+        <section className="padding">
+          <SpecialOffer />
+        </section>
+        <section className="padding bg-pale-blue">
+          <CustomerReviews />
+        </section>
+        <section className="padding-x sm:py-32 w-full">
+          <Subscribe />
+        </section>
       </main>
-      <footer className="padding-x padding-t pb-8 bg-black">Footer</footer>
+      <footer className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </footer>
     </>
   );
 };
