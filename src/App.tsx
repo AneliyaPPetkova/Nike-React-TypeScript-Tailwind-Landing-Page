a3a4752c222c013b02b90714788b46bda39a4832
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-10-19 13:09:39 +0300

    Create basic HTML5 structure for the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Nike App</h1>
+      <main className="relative">
+        <h1 className="text-3xl font-bold underline">Nike App</h1>
+        <section className="x1:padding-1 wide:padding-r padding-b">
+          Hero
+        </section>
+        <section className="padding">Popular Products</section>
+        <section className="padding">Super Quality</section>
+        <section className="padding-x py-10">Services</section>
+        <section className="padding">Special Offer</section>
+        <section className="padding bg-pale-blue">Customer Reviews</section>
+        <section className="padding-x sm:py-32 w-full">Subscribe</section>
+      </main>
+      <footer className="padding-x padding-t pb-8 bg-black">Footer</footer>
     </>
   );
-}
+};
 
 export default App;
